77dab0caca71d616ceda0caddcc8f5022fe4c0b0
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2025-11-17 14:03:22 +0600

    problem 1 & 2

--- solution.ts
+++ solution.ts
@@ -1,2 +1,23 @@
 // Problem 1
 
+type Value = string | boolean | number;
+const formatValue = (value: Value) => {
+  if (typeof value === "string") {
+    return value.toUpperCase();
+  }
+  if (typeof value === "boolean") {
+    return !value;
+  }
+  if (typeof value === "number") {
+    return value * 10;
+  }
+};
+
+const getLength = (value: string | unknown[]) => {
+  if (typeof value === "string") {
+    return value.length;
+  }
+  if (Array.isArray(value)) {
+    return value.length;
+  }
+};
